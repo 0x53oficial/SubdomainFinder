fe7e90eea736bd3ef274c323fd74b1105118f787
Samuel Silva <65719398+0x53oficial@users.noreply.github.com>
2020-09-27 03:44:15 -0300

    Update subdomainfinder.sh

--- subdomainfinder.sh
+++ subdomainfinder.sh
@@ -1,1 +1,13 @@
-for subname in $(cat $2);do host -t A $subname.$1 | grep -v "NXDOMAIN" | awk -F " " '{print $1}' ;done
+#!/bin/bash
+
+if [ "$1" == "" ]
+then
+	echo "Please, give me a domain :)"
+else
+	if [ "$2" == "" ]
+	then
+		echo "Please, give me a wordlist :)"	
+	else
+		for subname in $(cat $2);do host -t A $subname.$1 | grep -v "NXDOMAIN" | awk -F " " '{print $1}' ;done
+	fi
+fi
